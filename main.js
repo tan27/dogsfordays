b59d3124f28e60693721f9d5ee1895764573ad44
tan27 <tn2706@gmail.com>
2021-05-27 13:42:27 -0700

    Creation

---
+++ main.js
@@ -0,0 +1,36 @@
+
+
+
+async function start() {
+    const response = await fetch("https://dog.ceo/api/breeds/list/all")
+    const data = await response.json()
+    createBreedList(data.message)
+}
+
+start()
+
+function createBreedList(breedListChosenName) {
+    document.getElementById("breed").innerHTML = `
+    <select onchange="loadByBreed(this.value)">
+    <option>Select a dog breed</option>
+    ${Object.keys(breedListChosenName).map(function (breedChosen) {
+        return `<option>${breedChosen}</option>`
+    })}
+    </select>
+    `
+}
+
+async function loadByBreed(breednameChosen) {
+    if (breednameChosen != "Choose a dog breed") {
+        const response = await fetch(`https://dog.ceo/api/breed/${breednameChosen}/images`)
+        const data = await response.json()
+        createSlideshow(data.message)
+    }
+
+}
+
+function createSlideshow(dogPics) {
+    document.getElementById("slideshow").innerHTML = `
+    <div class="slide" style="background-image: url('${dogPics[0]}')"></div>`
+}
+
